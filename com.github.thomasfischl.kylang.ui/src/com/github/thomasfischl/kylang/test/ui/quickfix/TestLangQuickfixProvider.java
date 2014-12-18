39cc1be5a5bec266e214fa79faa9a214410bab9c
thomasfischl <fischl.thomas@gmail.com>
2014-12-18 20:27:31 +0100

    Improve Quickfixes
    Add userdefine keyword quickfix

--- com.github.thomasfischl.kylang.ui/src/com/github/thomasfischl/kylang/test/ui/quickfix/TestLangQuickfixProvider.java
+++ com.github.thomasfischl.kylang.ui/src/com/github/thomasfischl/kylang/test/ui/quickfix/TestLangQuickfixProvider.java
@@ -8,8 +8,10 @@ import org.eclipse.xtext.ui.editor.quickfix.Fix;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
 import org.eclipse.xtext.validation.Issue;
 
+import com.github.thomasfischl.kylang.test.testLang.Description;
 import com.github.thomasfischl.kylang.test.testLang.KeywordCall;
 import com.github.thomasfischl.kylang.test.testLang.KeywordDecl;
+import com.github.thomasfischl.kylang.test.testLang.KeywordList;
 import com.github.thomasfischl.kylang.test.testLang.KeywordMetatype;
 import com.github.thomasfischl.kylang.test.testLang.Model;
 import com.github.thomasfischl.kylang.test.testLang.PropertyDecl;
@@ -36,15 +38,13 @@ public class TestLangQuickfixProvider extends DefaultQuickfixProvider {
         keywordImpl.setMetatype(KeywordMetatype.SCRIPTED);
         keywordImpl.setScript(TestLangFactory.eINSTANCE.createKeywordScript());
         keywordImpl.getScript().setScriptType(ScriptType.JAVA);
-        keywordImpl.getScript().setClass("ImplementMeClass");
+        keywordImpl.getScript().setClass("com.github.thomasfischl.kylang.runtime.keywords.impl.UnimplementedKeyword");
 
-        if (keywordCall.getParameters() != null && keywordCall.getParameters().getParameters() != null) {
-          for (int i = 0; i < keywordCall.getParameters().getParameters().size(); i++) {
-            PropertyDecl prop = TestLangFactory.eINSTANCE.createPropertyDecl();
-            prop.setName("property" + (i + 1));
-            keywordImpl.getProperties().add(prop);
-          }
-        }
+        Description description = TestLangFactory.eINSTANCE.createDescription();
+        description.setText("'''TODO: Implement'''");
+        keywordImpl.setDesc(description);
+
+        defineParameters(keywordCall, keywordImpl);
 
         Model root = TestLangModelUtils.getRoot(keyword);
         root.getKeywords().add(keywordImpl);
@@ -52,9 +52,50 @@ public class TestLangQuickfixProvider extends DefaultQuickfixProvider {
     }
   }
 
+  private final class CreateUserKeywordHandler implements ISemanticModification {
+    @Override
+    public void apply(EObject keyword, IModificationContext context) {
+      if (keyword instanceof KeywordCall) {
+        KeywordCall keywordCall = (KeywordCall) keyword;
+
+        KeywordDecl keywordImpl = TestLangFactory.eINSTANCE.createKeywordDecl();
+        keywordImpl.setName(keywordCall.getName());
+        keywordImpl.setMetatype(KeywordMetatype.USERDEFINED);
+
+        Description description = TestLangFactory.eINSTANCE.createDescription();
+        description.setText("'''TODO: Implement'''");
+        keywordImpl.setDesc(description);
+
+        KeywordList keywordList = TestLangFactory.eINSTANCE.createKeywordList();
+
+        KeywordCall callKeyword = TestLangFactory.eINSTANCE.createKeywordCall();
+        callKeyword.setName("Unimplemented Keyword");
+        keywordList.getChildren().add(callKeyword);
+        keywordImpl.setKeywordlist(keywordList);
+
+        defineParameters(keywordCall, keywordImpl);
+
+        Model root = TestLangModelUtils.getRoot(keyword);
+        root.getKeywords().add(keywordImpl);
+      }
+    }
+
+  }
+
+  private void defineParameters(KeywordCall keywordCall, KeywordDecl keywordImpl) {
+    if (keywordCall.getParameters() != null && keywordCall.getParameters().getParameters() != null) {
+      for (int i = 0; i < keywordCall.getParameters().getParameters().size(); i++) {
+        PropertyDecl prop = TestLangFactory.eINSTANCE.createPropertyDecl();
+        prop.setName("property" + (i + 1));
+        keywordImpl.getProperties().add(prop);
+      }
+    }
+  }
+
   @Fix(TestLangJavaValidator.KEYWORD_NOT_EXISTS)
   public void fixName(final Issue issue, IssueResolutionAcceptor acceptor) {
     acceptor.accept(issue, "Create scripted keyword", "Create a new scripted keyword.", "upcase.png", new CreateKeywordHandler());
+    acceptor.accept(issue, "Create user keyword", "Create a new user keyword.", "upcase.png", new CreateUserKeywordHandler());
   }
 
 }
